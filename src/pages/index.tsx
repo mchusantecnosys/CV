9ed0724023b0e146bac9cd1f7c19ce9e7ed28e06
Mario <mchusan.tecnosys@gmail.com>
2025-05-19 20:52:47 -0500

    modified:   src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import { cn } from "@/lib/utils";
 //import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import perfil from './perfil1.jpg';
-import {Geist, Geist_Mono} from 'next/font/google';
 
 
 const cvData = {
