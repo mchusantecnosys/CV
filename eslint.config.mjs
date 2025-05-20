e9c5b8c68aa385a068ffc50e72969baf10acb660
Mario <mchusan.tecnosys@gmail.com>
2025-05-19 21:22:51 -0500

    modified:   eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ const eslintConfig = [
   ...compat.config({
     extends: ["next/core-web-vitals", "next/typescript"],
     "rules": {
-      "no-unused-vars": "off"
+      "@typescript-eslint/no-unused-vars": "off"
   },
 }),
 ];
